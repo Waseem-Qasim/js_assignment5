a325adeb97959b21353b2a637fe3d3a8c09143d4
waseem-qasim <waseemqasim@gmail.com>
2022-06-01 21:05:39 +0500

    js_assignment5 q1 to q5 is complete

---
+++ aap.js
@@ -0,0 +1,54 @@
+// assignment 5 Q-1
+
+// var arr = [[1,2], [3,4], [5,6]]
+// console.log (arr)
+
+
+// ========================================================
+// assignment 5 Q-2
+
+// var arr = [[0, 1, 2, 3 ], [1, 0, 1, 2], [2, 1, 0, 1]]
+// document.write (arr[0] + "</br>")
+// document.write (arr[1] + "</br>")
+// document.write (arr[2] + "</br>")
+
+
+
+// ========================================================
+// assignment 5 Q-3
+
+
+// for(var i = 0; i <= 10; i++){
+//         document.write(i + "<br/>")
+// }
+
+
+
+// ========================================================
+// assignment 5 Q-4
+
+// var inputNo = +prompt ("Enter Your Table Number")
+// var inputLength = +prompt ("Enter Your Table Length")
+
+// for (a = 1; a <= inputLength; a++) {
+//     document.write(inputNo + "x" + a + "=" + (a*inputNo) + "<br/>")
+    
+// }
+
+
+
+
+// ========================================================
+// assignment 5 Q-5
+
+var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+for (a = 0; a < fruits.length; a++){
+    document.write(fruits[a] + "</br>" + "</br>" )
+
+}
+
+for (a = 0; a < fruits.length; a++){
+    document.write("Element at index " + a + " is " + fruits[a] + "</br>" + "</br>" )
+
+}
